e633dfc19bf0d2f976b3fb92c86c94eeade41d07
dnegrey <dnegrey@gmail.com>
2016-04-19 00:01:45 -0400

    added render_post function

---
+++ R/render_post.R
@@ -0,0 +1,18 @@
+#' @title Render a post to HTML
+#' @description \code{render_post} renders a post to HTML
+#' @param post path to the post
+#' @return The post HTML file is created.
+#' @seealso \code{\link{new_post}}
+#' @examples 
+#' new_post("Hello, world!", Sys.Date(), "hello-world")
+#' render_post(post_dir(Sys.Date(), "hello-world"))
+#' @importFrom rmarkdown render
+#' @export 
+render_post <- function(post) {
+    if (!dir.exists(post)) {
+        stop("the post [", post, "] does not exist")
+    } else {
+        post <- paste(post, "index.Rmd", sep = "/")
+        render(post)
+    }
+}
